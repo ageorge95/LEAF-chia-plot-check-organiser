e4ab2be13dcb91b4fdd35e95b4d0941c421c00ba
George Daniel Arteni <arteni.george.daniel@gmail.com>
2021-11-16 21:32:37 +0200

    fixed a bug in update_version

--- _00_update_version.py
+++ _00_update_version.py
@@ -1,5 +1,5 @@
 import sys
 
 open("version.txt", "w").write(sys.argv[-1])
-spec_replacement=str(open("_00_GUI.spec", "r").readlines()).replace('name=\'_00_GUI\'', sys.argv[-1])
-open("_00_GUI.spec", "w").write(spec_replacement)
+spec_replacement=str(open("_00_GUI.spec", "r").readlines()).replace(r'name=''\'_00_GUI\'''', r'name=''\'{}\''''.format(sys.argv[-1]))
+open("_00_GUI.spec", "w").writelines(eval(spec_replacement))
