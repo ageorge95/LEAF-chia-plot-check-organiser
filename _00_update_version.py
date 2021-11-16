9ee6a2d832566cd2f137ccc14e629c46ec6b5b98
George Daniel Arteni <arteni.george.daniel@gmail.com>
2021-11-16 21:21:14 +0200

    updated the win auto-build workflow, again ...

--- _00_update_version.py
+++ _00_update_version.py
@@ -1,3 +1,5 @@
 import sys
 
 open("version.txt", "w").write(sys.argv[-1])
+spec_replacement=str(open("_00_GUI.spec", "r").readlines()).replace('name=\'_00_GUI\'', sys.argv[-1])
+open("_00_GUI.spec", "w").write(spec_replacement)
