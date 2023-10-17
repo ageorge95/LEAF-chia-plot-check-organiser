6320e38af41d9b1b9f1470a961c20b7773e9518d
ageorge95 <arteni.george.daniel@gmail.com>
2023-10-17 17:39:32 +0300

    enforced certain python versions compatibility

--- Install.bat
+++ Install.bat
@@ -1,14 +1,26 @@
 @echo off
 
+set python=%1
+IF "%python%"=="" set python="python"
+
 :: installation
 
 git submodule update --progress --init --recursive --force
 
-python -m venv venv
+%python% -m venv venv
 :: Windows doesn't allow the creation of symlinks without special priviledges, so hardlinks are created instead.
 mklink /h activate.bat venv\Scripts\activate.bat
 
 call activate.bat
+
+python install_helper.py || goto :error
+goto :all_good
+
+: error
+pause
+exit
+
+: all_good
 
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
